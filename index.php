a692e6ad3e8266b1e40334be4e585ab709cc7f86
dex.papa@gmail.com <dex.papa@gmail.com>
2018-01-19 22:15:09 +0530

    isUserRegistered()
    isUserRegistered is working perfectly fine, it has dependency databaseConnection

--- index.php
+++ index.php
@@ -2,18 +2,15 @@
   include 'UserActions.php';
 
   $User = new UserActions;
- // print_r ($User->databaseConnection('localhost','root','','admission2018'));
- 
-   if($User->isValidEmail('yash@gmail.com')){
-     /**
-      * Passed only true values
-      * Passing Spaces < > will break the function
-      * solution is to understan empty() function first
-      */
- 
-     echo $User->userRegistration('text@exampl.ecom','123','123');
-   }
-  //echo $User->userRegistration('y','2','2');
- 
- // print_r ($User->debugging());
+  $User->databaseConnection('localhost','root','','admission2018');
+  $email = '1234@gmail.com';
 
+  echo $User->isValidEmail($email). '<br>'; 
+  echo $User->isUserRegistered($email). '<br>';
+
+  if ($User->isUserRegistered($email)) {
+      echo 'User is already registered';
+  }
+  else {
+    echo 'User is not registered';
+  }
