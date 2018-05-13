ac20ccbf92825b1ada13a06eafeb779e2281a662
Yash Karanke <dex.papa@gmail.com>
2018-05-13 18:13:20 +0530

    LiNTER (#2)

--- linter.php
+++ linter.php
@@ -1,5 +1,16 @@
 <?php
+/**
+ * Linter test file, pleases ignore 
+ */
     echo 'Hello world';
             echo 'Hello world';
                 echo 'Hello world';
-            
+                echo 'Hello world';
+                echo 'Hello world';
+                echo 'Hello world';
+                        echo 'Hello world';
+                echo 'Hello world';
+            echo 'Hello world';
+                echo 'Hello world';
+                echo 'Hello world';
+                            
